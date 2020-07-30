59bda8e467227d2ecac25e4d9c7b63fe410162dd
StivenVelasquez <62726675+StivenVelasquez@users.noreply.github.com>
2020-07-30 09:47:17 -0500

    Se añaden los 5 cuerpos a la escena

--- Parcial Final-20192-Informatica2/Parcial_Final/mainwindow.cpp
+++ Parcial Final-20192-Informatica2/Parcial_Final/mainwindow.cpp
@@ -1,11 +1,33 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setWindowTitle(tr("SIMULACIÓN")); //Titulo
+    cronometro=new QTimer(this);    //crea el timer
+    scene=new QGraphicsScene(this);   // crea la escena
+    scene->setSceneRect(-200,-200,400,500);
+    ui->graphicsView->setScene(scene);
+    ui->graphicsView->setStyleSheet("background-color:blue");//Para color del graphicsView
+    //Se crean 4 cuerpos para la simulación
+    Cuerpos[0]=new Planetas(0,0,0,0,50000,15);
+    Cuerpos[1]=new Planetas(-5000,0,0,-2,70,15);
+    Cuerpos[2]=new Planetas(5000,0,0,2,70,15);
+    Cuerpos[3]=new Planetas(0,-5000,2,0,70,15);
+    Cuerpos[4]=new Planetas(0,5000,-2,0,70,15);
+    //Se añaden a la escena
+    scene->addItem(Cuerpos[0]);
+    scene->addItem(Cuerpos[1]);
+    scene->addItem(Cuerpos[2]);
+    scene->addItem(Cuerpos[3]);
+    scene->addItem(Cuerpos[4]);
+
+    cronometro->start(10);
+
+    connect(cronometro,SIGNAL(timeout()),this,SLOT(actualizar()));
 }
 
 MainWindow::~MainWindow()
--- Parcial Final-20192-Informatica2/Parcial_Final/mainwindow.h
+++ Parcial Final-20192-Informatica2/Parcial_Final/mainwindow.h
@@ -19,9 +19,14 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+  //  void on_pushButton_clicked(); //Para cuando se pulse el boton
+    void actualizar(); //Actualizacion
+
 private:
     Ui::MainWindow *ui;
     QGraphicsScene *scene;
+    QTimer *cronometro;
     Planetas *Cuerpos[5];
 };
 
